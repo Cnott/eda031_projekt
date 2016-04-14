d6685d8b58720674081c511fe551edf42c3fb76a
andres <andresthor@gmail.com>
2016-04-14 12:00:32 +0200

    added database.h interface

---
+++ src/server/database.h
@@ -0,0 +1,44 @@
+/* ---------------------------------------------------------------------------
+** This software is in the public domain, furnished "as is", without technical
+** support, and with no warranty, express or implied, as to its usefulness for
+** any purpose.
+**
+** database.h
+** <very brief file description>
+**
+** Authors: Andres Saemundsson, Anton Friberg, Oscar Gunneson
+** -------------------------------------------------------------------------*/
+#ifndef DATABASE_H
+#define DATABASE_H
+
+/*-------------------------------------
+    I N C L U D E S
+-------------------------------------*/
+#include <string>
+
+/*-------------------------------------
+    D E C L A R A T I O N S
+-------------------------------------*/
+// not recommended -> using namespace std;
+
+/*-------------------------------------
+    C L A S S   D E F
+-------------------------------------*/
+
+class Database {
+public:
+  Database();
+  virtual ~Database();
+
+  virtual void saveNewsgroup(std::string ngName);
+  virtual void saveArticle (int ngID, std::string artName, std::string author,
+                            std::string text);
+  virtual void listNewsGroup();
+  virtual void listArticles(int nwId);
+  virtual void getArticle(int ngID, int artID);
+
+private:
+
+};
+
+#endif
