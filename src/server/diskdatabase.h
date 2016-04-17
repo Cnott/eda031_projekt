86fb4d946e7abff0288288e76d05dd14fa75eb85
andres <andresthor@gmail.com>
2016-04-17 08:12:07 +0200

    formatting in diskdatabase.h. A few more fixes.

--- src/server/diskdatabase.h
+++ src/server/diskdatabase.h
@@ -23,8 +23,6 @@
 #include <fstream>
 #include <sstream>
 #include <list>
-
-//#include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
@@ -32,18 +30,13 @@
 #include "database.h"
 
 /*-------------------------------------
-    D E C L A R A T I O N S
--------------------------------------*/
-
-
-/*-------------------------------------
     C L A S S   D E F
 -------------------------------------*/
 
 class DiskDatabase : public Database {
 public:
-  DiskDatabase();
-  ~DiskDatabase() { closedir(dbRootDir);}
+  DiskDatabase          ();
+  ~DiskDatabase         () { closedir(dbRootDir); }
 
   bool addArticle       ( unsigned int ngId,  std::string title,
                           std::string author, std::string text      );
@@ -60,8 +53,6 @@ public:
   std::vector<Newsgroup>  listNewsgroups    (                       );
   std::vector<Article>    listArticles      ( unsigned int ngId     );
 
-
-
 private:
   void print              ();
   void initDatabase       ();
@@ -75,7 +66,7 @@ private:
 
   const char* dbRoot        = "database/";
   unsigned char isDir       = 0x4;
-  struct dirent* DirEntry;
+  DIR* dbRootDir;
 };
 
 #endif
