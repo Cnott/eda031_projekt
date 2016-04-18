7c3e2165e127392e27bd5cd919f575ffd12f7236
AntonFriberg <anton.friberg@outlook.com>
2016-04-18 14:19:09 +0200

    Try catch added to server

--- src/server/newsserver_disk/newsserver_disk.cc
+++ src/server/newsserver_disk/newsserver_disk.cc
@@ -2,8 +2,14 @@
 //#include "../database.h"
 #include "diskdatabase.h"
 
+using namespace std;
 int main(int argc, char* argv[]) {
   DiskDatabase db;
   NewsServer ns(argc, argv);
-  ns.run(db);
+  try {
+    ns.run(db);
+  } catch (exception& e) {
+    cout << " Error: Something went wrong. Server is closing." << endl;
+    exit(1);
+  }
 }
--- src/server/newsserver_memory/newsserver_memory.cc
+++ src/server/newsserver_memory/newsserver_memory.cc
@@ -1,9 +1,16 @@
 #include "../newsserver.h"
 //#include "../database.h"
 #include "memdatabase.h"
-
+#include <exception>
+using namespace std;
 int main(int argc, char* argv[]) {
   MemDatabase db;
   NewsServer ns(argc, argv);
-  ns.run(db);
+
+  try {
+    ns.run(db);
+  } catch (exception& e) {
+    cout << " Error: Something went wrong. Server is closing." << endl;
+    exit(1);
+  }
 }
