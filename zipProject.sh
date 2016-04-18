7b47cd55e452f3b25a6e3adfe78ceda1226d565d
AntonFriberg <anton.friberg@outlook.com>
2016-04-18 23:04:49 +0200

    Removed database from zipProject

--- zipProject.sh
+++ zipProject.sh
@@ -2,6 +2,6 @@
 
 echo "Creating tar of relevant project files..."
 
-tar -czf eda031_atn08asa_ama10ogu_tfy12afr.tar.gz articles/ database/ src/ makefile README.md
+tar -czf eda031_atn08asa_ama10ogu_tfy12afr.tar.gz articles/ src/ makefile README.md
 
 echo "...done!"
